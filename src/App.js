2fb1e418f70e009d2d1b18a312038e1b9dbf01cb
Cole Johnston <79526633+colepjohn@users.noreply.github.com>
2021-06-21 19:14:38 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Sortvis from './Sortvis/Sortvis';
+
+function App() {
+  return (
+    <div className="App">
+      <Sortvis></Sortvis>
+    </div>
+  );
+}
+
+export default App;
